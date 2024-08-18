3e853d7509bd4162c093832a4fc2f45c5ded97b1
ada-rsh-s <aadarsh04ss@gmail.com>
2024-08-18 08:48:15 +0530

    Basic design setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
 import Fallback from "./components/Fallback";
 import Login from "./pages/Login";
+import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ProtectedRoute from "./pages/ProtectedRoute";
+import Sharedlayout from "./pages/dashboard/SharedLayout";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Sharedlayout />
+            </ProtectedRoute>
+          }
+        >
+          <Route index element={<h1>Home</h1>} />
+          <Route path="dept-exams" element={<h1 >dept table</h1>} />
+          
+        </Route>
+
+        <Route path="/login" element={<Login />} />
         <Route path="*" element={<Fallback />} />
       </Routes>
     </BrowserRouter>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,68 @@
-import "../App.css";
+import "../styles/Login.css";
+import { Button, Checkbox, Form, Input, Flex } from "antd";
 
+
+import FlexContainer from "../utils/FlexContainer";
 const Login = () => {
   return (
- <h1>login</h1>
+    <>
+      <div clasName="loginHeader">
+        <img className="loginLogo" src="../Logo.png" alt="" srcset="" />
+      </div>
+      <div className="login">
+        <FlexContainer>
+          <div className="loginVector">
+            <img src="../loginVector.png" alt="" srcset="" />
+            <p>Organize exams with ease !</p>
+          </div>
+          <div className="loginDivider"></div>
+          <Form
+            name="login"
+            initialValues={{ remember: true }}
+            style={{ maxWidth: 360 }}
+          >
+            <Form.Item
+              name="username"
+              rules={[
+                { required: true, message: "Please input your Username!" },
+              ]}
+            >
+              <Input
+                prefix={<img src="../loginPerson.png" alt="" srcset="" />}
+                placeholder="Username"
+              />
+            </Form.Item>
+            <Form.Item
+              name="password"
+              rules={[
+                { required: true, message: "Please input your Password!" },
+              ]}
+            >
+              <Input
+                prefix={<img src="../loginPassword.png" alt="" srcset="" />}
+                type="password"
+                placeholder="Password"
+              />
+            </Form.Item>
+            <Form.Item>
+              <Flex justify="space-between" align="center">
+                <Form.Item name="remember" valuePropName="checked" noStyle>
+                  <Checkbox>Remember me</Checkbox>
+                </Form.Item>
+                <a href="">Forgot password</a>
+              </Flex>
+            </Form.Item>
+
+            <Form.Item>
+              <Button block type="primary" htmlType="submit">
+                Log in
+              </Button>
+              or <a href="">Register now!</a>
+            </Form.Item>
+          </Form>
+        </FlexContainer>
+      </div>
+    </>
   );
 };
 
