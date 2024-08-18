48580fc52c507c23db7fd6e9b3eced797ed8a61b
Alan Jose Santo <realityaudiostudio@gmail.com>
2024-08-18 11:31:27 +0530

    Navbar fixed

--- src/components/NavigationTab.jsx
+++ src/components/NavigationTab.jsx
@@ -1,6 +1,8 @@
 import { Segmented } from "antd";
+import "./navtab.css";
 const NavigationTab = () => (
   <Segmented
+  className="navigationTab"
     options={["Home", "Exam Dept", "Slots", "Classes", "Yearly"]}
     onChange={(value) => {
       console.log(value); // string
