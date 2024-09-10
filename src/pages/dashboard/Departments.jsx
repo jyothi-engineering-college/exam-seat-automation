88bbce6ab81eb6b3a15b28452487790bd6f7dde0
ada-rsh-s <aadarsh04ss@gmail.com>
2024-09-10 06:59:12 +0530

    date picker for year added

--- src/pages/dashboard/Departments.jsx
+++ src/pages/dashboard/Departments.jsx
@@ -1,8 +1,44 @@
-import TableContainer from '../../components/TableContainer';
+import { Button, DatePicker } from "antd";
+import TableContainer from "../../components/TableContainer";
+import { ConfigProvider } from "antd";
+import { Space } from "antd";
+import { message } from "antd";
+import { useEffect } from "react";
 
 const Departments = () => {
-  return <TableContainer />;
+  const [messageApi, contextHolder] = message.useMessage();
+  const yearChanged = (date) => {      
+      messageApi.open({
+        type: "success",
+        content: `Academic year changed to ${date.$y}`,
+      });
+    };
 
-}
+  return (
+    <>
+      <div className="academicYear">
+        <p>Select Academic year</p>
+        <ConfigProvider
+          theme={{
+            token: {
+              colorText: "#07314a",
+              fontWeightStrong: 800,
+              colorPrimary: "#07314a",
+              colorBgContainer: "#f0f9ff",
+              colorBgElevated: "#f0f9ff",
+            },
+          }}
+        >
+          <Space>
+            {contextHolder}
 
-export default Departments
+            <DatePicker onChange={yearChanged} picker="year" />
+          </Space>
+        </ConfigProvider>
+      </div>
+      <TableContainer />
+    </>
+  );
+};
+
+export default Departments;
